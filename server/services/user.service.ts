ce017d70cfc0d89505ea0ae3cf2ad38606eee6d4
Dana Cohen <dana.co@moonactive.com>
2025-01-05 21:59:06 +0200

    sort getAllUsers

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -42,6 +42,30 @@ export const UserService = {
     },
 
     async getAllUsers() {
-        return User.find();
+        return User.aggregate([
+            {
+                $lookup: {
+                    from: 'chats',
+                    localField: '_id',
+                    foreignField: 'participants',
+                    as: 'chats'
+                }
+            },
+            {
+                $addFields: {
+                    hasChats: { $gt: [{ $size: '$chats' }, 0] },
+                    lastUpdated: { $max: '$chats.lastUpdated' }
+                }
+            },
+            {
+                $sort: { lastUpdated: -1 }
+            },
+            {
+                $project: {
+                    chats: 0,
+                    hasChats: 0
+                }
+            }
+        ]);
     }
 };
