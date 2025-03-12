f8693dfd50d12191bfbd9161a500e6e7aa463be4
Revital Frid <revitalf512@gmail.com>
2025-03-12 20:38:10 +0200

    fix: try using server root path

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -11,16 +11,17 @@ router.use("/record", recordRoutes);
 router.use("/upload", uploadRoutes);
 router.use("/user", userRoutes);
 
+const serverRoot = path.resolve(__dirname, '..'); // Since `index.ts` is inside `server/routes/`, one level up gets `server/`
 
 // Serve the uploaded and default files statically
-router.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
-router.use('/default-files', express.static(path.resolve(__dirname, '../default-files')));
+router.use('/uploads', express.static(path.join(serverRoot, 'uploads')));
+router.use('/default-files', express.static(path.join(serverRoot, 'default-files')));
 
 // Serve React static files
-router.use(express.static(path.resolve(__dirname, '../../client/dist')));
+router.use(express.static(path.join(__dirname, '../../../client/dist')));
 
 router.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
 
 export default router;
