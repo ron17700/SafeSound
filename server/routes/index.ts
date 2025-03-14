2c978d7ac4574db12fb5b8b63d526dd2c39045b9
Revital Frid <revitalf512@gmail.com>
2025-03-14 18:04:33 +0200

    fix: return routes back to normal

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -11,48 +11,15 @@ router.use("/record", recordRoutes);
 router.use("/upload", uploadRoutes);
 router.use("/user", userRoutes);
 
-// Get absolute path dynamically for server directory
-const serverRoot = path.resolve(__dirname, '..'); // Points to 'server/'
-
-// Serve static files
-const uploadsPath = path.join(serverRoot, 'uploads');
-const defaultFilesPath = path.join(serverRoot, 'default-files');
-
-console.log("Serving uploads from:", uploadsPath);
-console.log("Serving default files from:", defaultFilesPath);
-router.use('/uploads', express.static(uploadsPath, {
-    setHeaders: (res) => {
-        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-        res.setHeader('Expires', '0');
-        res.setHeader('Pragma', 'no-cache');
-        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow external access
-    }
-}));
-router.use('/default-files', (req, res, next) => {
-    console.log(`🔍 Serving: ${req.originalUrl}`);
-    next();
-}, express.static(defaultFilesPath, {
-    fallthrough: false
-}));
-
-router.get('/test-image', (req, res) => {
-    const imagePath = path.join(defaultFilesPath, 'default-record-image.jpg');
-    res.setHeader('Content-Type', 'image/jpeg'); // ✅ Explicitly set content type
-    res.sendFile(imagePath, (err) => {
-        if (err) {
-            console.error("❌ Error sending file:", err);
-            // res.status(err.status).end();
-        }
-    });
-});
+// Serve the uploaded and default files statically
+router.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+router.use('/default-files', express.static(path.join(__dirname, '../default-files')));
 
 // Serve React static files
-router.use(express.static(path.join(serverRoot, '../client/dist')));
+router.use(express.static(path.join(__dirname, '../../../client/dist')));
 
-router.get('*', (req, res, next) => {
-    if (req.path.startsWith('/default-files') || req.path.startsWith('/uploads')) {
-        return next(); // ✅ Let Express handle static files
-    }
-    res.sendFile(path.join(serverRoot, '../client/dist/index.html'));
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
+
 export default router;
