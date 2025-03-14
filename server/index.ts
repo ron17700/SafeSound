b8d752798aac85b91055c69a4e9efdc890713308
Ron Yaari <ryaari@salesforce.com>
2025-03-14 17:37:47 +0200

    Fix client dist path

--- server/index.ts
+++ server/index.ts
@@ -44,7 +44,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(express.static(path.resolve(__dirname, "./uploads")));
 app.use(express.static(path.resolve(__dirname, "./default-files")));
-app.use(express.static(path.resolve(__dirname, "../client/dist")));
+app.use(express.static(path.resolve(__dirname, "../../client/dist")));
 
 app.use("/", mainRoutes);
 app.use(errorHandler);
